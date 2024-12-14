94e53c7009a8e05220f3fb956c472ef9f07fb0f9
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2024-12-14 09:43:03 +0530

    commit

--- src/components/projects/projectHero.tsx
+++ src/components/projects/projectHero.tsx
@@ -7,7 +7,7 @@ import { PinContainer } from "../ui/3d-pin";
         <div className="h-full w-full grid md:grid-cols-3 gap-20 mt-28 mb-20">
             <PinContainer
                 title="/ui.aceternity.com"
-                href="https://ui.aceternity.com"
+                href="https://github.com/krishnabakshi2455/PortFolio_6/blob/main/src/components/ui/3d-pin.tsx"
             >
                 <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem]">
                     <h3 className="max-w-xs !pb-2 !m-0 font-bold text-base text-slate-100">
